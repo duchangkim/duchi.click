5dd03d365dfad0a0a952bd432b9732618c9add1e
duchangkim <duchang.dev@gmail.com>
2024-07-11 13:41:01 +0900

    💄 Change footer style

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <footer className="mt-auto border-t border-neutral-200 bg-neutral-50 dark:border-zinc-700 dark:bg-zinc-800">
       <Container>
-        <div className="flex flex-col items-center py-10 text-zinc-600 dark:text-zinc-300">
+        <div className="flex flex-col items-center py-7 text-sm text-zinc-500 dark:text-zinc-400">
           &copy; {today.getFullYear()} duchi. All rights reserved.
         </div>
       </Container>
