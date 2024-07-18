9d06a50aa79af0b951523515f75e701778f073c5
duchangkim <duchang.dev@gmail.com>
2024-07-18 21:21:48 +0900

    ✨ Improve SEO things

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
+  env: {
+    BASE_URL:
+      process.env.NODE_ENV === 'production' ? 'https://duchi.click' : 'http://localhost:3000',
+  },
 };
+
+module.exports = nextConfig;
