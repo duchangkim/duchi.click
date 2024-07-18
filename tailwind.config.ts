ac15003939abce702d4dc3815169be29b323e6f5
duchangkim <duchang.dev@gmail.com>
2024-07-18 14:29:32 +0900

    💄 Fix container sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1024px',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
