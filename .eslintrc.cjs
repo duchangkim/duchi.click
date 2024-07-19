a5308390f0a4d78fc9415f25470cdca79e991e88
duchangkim <duchang.dev@gmail.com>
2024-07-19 19:25:27 +0900

    🚨 Fix eslint and vscode settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,7 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // next.js의 metadata 등등 함께 export 해야하는 경우가 있어서 off로 설정합니다.
+    'react-refresh/only-export-components': "off",
   },
 };
