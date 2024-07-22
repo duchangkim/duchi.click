113ca60fe2757609a74c62fc877c7be45cfad6cc
duchangkim <duchang.dev@gmail.com>
2024-07-22 14:12:52 +0900

    💄 Change post title position

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -14,10 +14,10 @@ interface Props {
 export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
       <div className="mb-8 sm:mx-0 md:mb-16">
         <CoverImage title={title} src={coverImage} />
       </div>
+      <PostTitle>{title}</PostTitle>
       <div className="mx-auto max-w-2xl">
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
