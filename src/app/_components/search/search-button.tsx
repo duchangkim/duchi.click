d8ec6d5157707c4f7505b1e72649a001825be0a8
duchangkim <duchang.dev@gmail.com>
2024-08-19 20:14:54 +0900

    💄 darkmode

--- src/app/_components/search/search-button.tsx
+++ src/app/_components/search/search-button.tsx
@@ -8,15 +8,17 @@ export const SearchButton = () => {
     <button
       type="button"
       className={classNames(
-        'flex items-center gap-2 rounded-md px-2 py-1 hover:bg-neutral-100 active:bg-neutral-200',
+        'flex items-center gap-2 rounded-md px-2 py-1 hover:bg-neutral-100 active:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:active:bg-neutral-800',
         {
-          'bg-neutral-200': showsSearch,
+          'bg-neutral-200 dark:bg-neutral-800': showsSearch,
         },
       )}
       onClick={() => setShowsSearch(true)}
     >
       <span>Search</span>
-      <span className="rounded-sm border bg-white px-1 text-xs text-neutral-500">/</span>
+      <span className="rounded-sm border bg-white px-1 text-xs text-neutral-500 dark:border-neutral-600 dark:bg-neutral-900">
+        /
+      </span>
     </button>
   );
 };
