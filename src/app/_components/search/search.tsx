bccff5302c2bff7ff736e9cb18cc2fc1a6a97353
duchangkim <duchang.dev@gmail.com>
2024-08-19 20:23:49 +0900

    💄 Improve search design

--- src/app/_components/search/search.tsx
+++ src/app/_components/search/search.tsx
@@ -121,7 +121,7 @@ export const Search = () => {
         <div className="pointer-events-none fixed left-0 top-0 z-auto flex h-full w-full justify-center">
           <div
             role="dialog"
-            className="pointer-events-auto mb-[100px] mt-[100px] flex h-fit max-h-[80%] min-w-[80%] max-w-[80%] flex-col overflow-y-auto rounded-md border border-zinc-600 bg-white shadow-md dark:border-zinc-700 dark:bg-neutral-900 lg:min-w-[60%] lg:max-w-[60%]"
+            className="pointer-events-auto mb-[100px] mt-[100px] flex h-fit max-h-[80%] min-w-[80%] max-w-[80%] flex-col overflow-y-auto rounded-md border border-zinc-600 bg-white shadow-md dark:border-zinc-700 dark:bg-neutral-900 lg:min-w-[1024px] lg:max-w-[1024px]"
           >
             <div className="sticky top-0 bg-white p-5 dark:bg-neutral-900">
               <input
@@ -133,18 +133,20 @@ export const Search = () => {
                 onChange={handleSearchKeywordChange}
               />
             </div>
-            {results && results.length > 0 && (
-              <ul className="flex flex-col gap-4 px-5 pb-5">
-                {results.map((result: any) => (
-                  <li
-                    className="min-h-[100px] w-full rounded-md border border-zinc-200 px-2 py-1.5 hover:bg-zinc-100 dark:border-zinc-700 dark:hover:bg-neutral-800"
-                    key={result.id}
-                  >
-                    <SearchResult {...result} />
-                  </li>
-                ))}
-              </ul>
-            )}
+            {
+              results && results.length > 0 ? (
+                <ul className="flex flex-col gap-4 px-5 pb-5">
+                  {results.map((result: any) => (
+                    <li
+                      className="min-h-[100px] w-full rounded-md border border-zinc-200 px-2 py-1.5 hover:bg-zinc-100 dark:border-zinc-700 dark:hover:bg-neutral-800"
+                      key={result.id}
+                    >
+                      <SearchResult {...result} />
+                    </li>
+                  ))}
+                </ul>
+              ) : null /* 검색 결과 없는 경우 */
+            }
           </div>
         </div>
       </div>
